6a57b2f5d1abbca86fab90ecf24ba3a4441eca3e
yaroslavpeshkov <159679641+yaroslavpeshkov@users.noreply.github.com>
2024-02-27 22:10:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Developers: Khramchikhina A., Peshkov Y., Sanzhanova A., Yurshenaite A.
 #
 
-import main as ru
+import ru_local as ru
 
 
 def main():
@@ -14,18 +14,18 @@ def main():
     mass_skin = float(input(ru.MASS_SKIN))
     volume_l = float(input(ru.VOLUME_L))
     t_outside_c = float(input(ru.T_OUTSIDE_C))
-    pressure = 10 ** 5
-    molar_mass = 29 * (10 ** (-3))
+    pressure = 10**5
+    molar_mass = 29 * (10**(-3))
     gas_constant = 8.31
     specific_heat = 1_000
     
     t_outside_k = t_outside_c + 273.15
-    volume_m3 = volume_l * 10 ** (-3)
-    t_inside_k = (t_outside_k * molar_mass * volume_m3 * pressure) / \
-                 (molar_mass * pressure * volume_m3 - mass_skin * t_outside_k * gas_constant)
+    volume_m3 = volume_l * (10**(-3))
+    t_inside_k = (t_outside_k*molar_mass*volume_m3*pressure) / \
+                 (molar_mass*pressure*volume_m3 - mass_skin*t_outside_k*gas_constant)
     t_inside_c = t_inside_k - 273.15
-    t_inside_f = t_outside_c * 9/5 + 32
-    mass_air = pressure * molar_mass * volume_m3 / gas_constant * t_outside_k
+    t_inside_f = t_outside_c*9/5 + 32
+    mass_air = pressure*molar_mass*volume_m3 / gas_constant*t_outside_k
     delta_t = t_inside_k - t_outside_k
     quantity_heat = specific_heat * mass_air * delta_t
         
